5b44ed9eefdb58e8cd1cfacb0a0c0fcd6663ae01
Sebana02 <sebana@ucm.es>
2021-10-22 16:59:14 +0200

    añadido minijuego guille

--- src/game.js
+++ src/game.js
@@ -1,11 +1,30 @@
-import BaseScene from "./scene.js";
+import Boot from './boot.js';
+import End from './end.js';
+import Platform from './platform.js';
+import Level from './scene.js';
 
-new Phaser.Game({
+/**
+ * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
+ * la clase Game de Phaser, encargada de crear e iniciar el juego.
+ */
+let config = {
     type: Phaser.CANVAS,
     canvas: document.getElementById("juego"),
-    width: 800,
-    height: 400,
-    scene: [BaseScene]
-});
+    width:  1000,
+    height: 500,
+    scale: {
+        // mode: Phaser.Scale.FIT,  
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+    },
+    pixelArt: true,
+    scene: [Boot, Level, End],
+    physics: { 
+        default: 'arcade', 
+        arcade: { 
+            gravity: { y: 400 }, 
+            debug: false 
+        } 
+    }
+};
 
-document.getElementById("juego").focus();
+new Phaser.Game(config);
