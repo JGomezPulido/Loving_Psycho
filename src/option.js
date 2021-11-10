9c22f87c97002563dd598408b1ae0b53e6f15c71
Edu-git1 <eorellan@ucm.es>
2021-11-10 16:52:50 +0100

    Clase Option

--- src/option.js
+++ src/option.js
@@ -1,3 +1,17 @@
-export default class Option{
-    
+export default class Option extends Phaser.GameObjects.Text{
+
+    constructor(scene, text, maxS, minS, score, id_obj, x, y){
+        this.maxScore = maxS;
+        this.minScore = minS;
+        this.score = score;
+        this.id_obj = id_obj;
+        this.scene = scene;
+        this.setInteractive();
+        this.on("pointerdown", this.clickOption(), this);
+    }
+
+    clickOption(){
+        this.scene.events.emit('optionClicked', id_obj)
+
+    }
 }
