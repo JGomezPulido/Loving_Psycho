deb08ea38c6e59c30bc022a258d304a60a7533e0
Edu-git1 <eorellan@ucm.es>
2021-11-22 18:31:35 +0100

    Background configurado

--- src/background.js
+++ src/background.js
@@ -1,3 +1,12 @@
 export default class Background extends Phaser.Image{
-    
+    constructor(scene, x, y){
+        super(scene, x, y, 'blackBackground');
+        this.setScale(this.height,this.height);
+        this.scene.events.on('changeBackground', this.changeBackground, this);
+        this.scene.add.existing(this);
+    }
+        
+    changeBackground(background){
+        this.setImage(background);
+    }
 }
--- src/boot.js
+++ src/boot.js
@@ -17,6 +17,7 @@ export default class Boot extends Phaser.Scene {
     this.load.image('sonrojada','sonrojada.png');
     this.load.image('horrorizada','horrorizada.png');
     this.load.image('feliz','feliz.png');
+    this.load.image('blackBackground','background.png');
     this.load.json('tree','../../src/tree.json');
   }
   
