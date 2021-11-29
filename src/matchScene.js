16ec5e52c0f173ea2e0d33dd7a863569d4e7ca44
jgomez18UCM <jgomez18@ucm.es>
2021-11-29 16:25:13 +0100

    Refactorizacion MatchScene

--- src/matchScene.js
+++ src/matchScene.js
@@ -6,15 +6,22 @@ export default class MatchScene extends Phaser.Scene {
     constructor() {
       super({ key:'matchScene'});
     }
+
     init(data){
       this.match = data.match;
     }
+
     create(){
       
       let canvasW = document.getElementById("juego").width;
       let canvasH = document.getElementById("juego").height;
-      this.background = new Background(this,canvasW/2, canvasH/2);     
-      this.text = this.add.text(canvasW/2, canvasH/2, 'La cita ha terminado, Match: ' + this.match + "%.");
+      this.background = new Background(this,canvasW/2, canvasH/2); 
+      let textString = "La cita ha terminado. Match: " + this.match + "%";
+      this.setText(canvasW/2, canvasH/2, textString);
+    }
+
+    setText(x, y, string){
+      this.text = this.add.text(x, y, string);
       this.text.setOrigin(0.5,0.5);
       this.text.setFontSize(30);
     }
