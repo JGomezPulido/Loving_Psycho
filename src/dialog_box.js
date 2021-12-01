269aa4e94ca9104638d9329d4fb35d269fb59ef3
jgomez18UCM <jgomez18@ucm.es>
2021-12-01 21:39:50 +0100

    Refactorizacion dialogbox (no terminado)

--- src/dialog_box.js
+++ src/dialog_box.js
@@ -1,50 +1,28 @@
+/**
+ * Representa un cuadro de diálogo.
+ */
 export default class DialogBox extends Phaser.GameObjects.Container{
     constructor(scene, x, y, w, h, inter, dialog_manager){
         super(scene, x, y);
         
         this.scene.events.on('optionsStart', this.showOptionDialog, this);
-
-        //Creación del cuadro de diálogo
-        this.dialogeBackground = new Phaser.GameObjects.Sprite(this.scene, 0, -50, 'cuadroDialogo');
-        this.dialogeBackground.setScale(w, h);
-        let sizeH = (h * this.dialogeBackground.height);
-        let sizeW = (w * this.dialogeBackground.width);
-        this.dialogeBackground.setOrigin(0.5, 0.5);
-        
-        //Creación del texto
-        const SPACING = 45;
-        this.text = new Phaser.GameObjects.Text(this.scene, this.dialogeBackground.x - sizeW / 2 + SPACING, this.dialogeBackground.y - sizeH / 2 + SPACING / 2, '');
-        this.text.setWordWrapWidth(sizeW - SPACING * 2);
-        this.text.setFontStyle('bold');
-        this.text.setFontSize(28);
-        this.text.setColor('#000'),
-
-        //Creación del container
-        this.add(this.dialogeBackground);
-        this.add(this.text);
-        if (inter)
-          this.setPosition(this.x, this.y - sizeH / 2);           
-        else{
-          this.setPosition(this.x, this.y + 100);
-          this.setVisible(false);
-          this.setActive(false);
-        }          
-        this.scene.add.existing(this);    
+        this.initContainer(inter, w, h);
+           
         
         //Variables para gestionar la escritura del diálogo
-        this.parrafo =  dialog_manager.getActualNode().dialogs;
-        this.actParrafo = '';
-        this.cont = 0;
-        this.delay = 0;
-        this.textoEscrito = false;
-        this.textSpeed = 30; 
+        this._parrafo =  dialog_manager.getActualNode().dialogs;
+        this._actParrafo = '';
+        this._cont = 0;
+        this._delay = 0;
+        this._textoEscrito = false;
+        this._textSpeed = 30; 
 
         //Convierto el cuadro en un botón para gestionar el texto
-        this.dialogeBackground.setInteractive();
-        this.dialogeBackground.on('pointerdown', pointer => {
-            if (!this.textoEscrito){              
-              this.text.setText(this.parrafo);
-              this.textoEscrito = true;
+        this._dialogeBackground.setInteractive();
+        this._dialogeBackground.on('pointerdown', pointer => {
+            if (!this._textoEscrito){              
+              this._text.setText(this._parrafo);
+              this._textoEscrito = true;
               this.scene.events.emit('writtenText');             
             }
             else if (inter){
@@ -54,7 +32,7 @@ export default class DialogBox extends Phaser.GameObjects.Container{
                 this.scene.events.emit('optionsStart', dialog_manager.getActualNode());
               }
               else{
-                this.parrafo = dialog_manager.getActualNode().dialogs;
+                this._parrafo = dialog_manager.getActualNode().dialogs;
               }
             }
           });
@@ -64,6 +42,51 @@ export default class DialogBox extends Phaser.GameObjects.Container{
           this.b = inter;
     }
 
+    createDialogBox(w, h){
+      let dialogeBackground = new Phaser.GameObjects.Sprite(this.scene, 0, -50, 'cuadroDialogo');
+      dialogeBackground.setScale(w, h);
+      dialogeBackground.setOrigin(0.5, 0.5);
+      return dialogeBackground
+    }
+
+    createText(){
+      let SPACING = 45;
+      let x = this._dialogeBackground.x - this._sizeW / 2 + SPACING;
+      let y = this._dialogeBackground.y - this._sizeH / 2 + SPACING / 2;
+      let text = new Phaser.GameObjects.Text(this.scene, x, y, '');
+      text.setWordWrapWidth(this._sizeW - SPACING * 2);
+      text.setFontStyle('bold');
+      text.setFontSize(28);
+      text.setColor('#000');
+      return text;
+    }
+
+    initContainer(inter, w, h){
+      //Creación del cuadro de diálogo
+      this._dialogeBackground = this.createDialogBox(w,h);
+
+      //Cálculo del size.
+      this._sizeH = (h * this._dialogeBackground.height);
+      this._sizeW = (w * this._dialogeBackground.width);
+      
+      //Creación del texto
+      this._text = this.createText();
+
+      //Creación del container
+      this.add(this._dialogeBackground);
+      this.add(this._text);
+      if (inter)
+        this.setPosition(this.x, this.y - this._sizeH / 2);           
+      else{
+        let offsetY = 100;
+        this.setPosition(this.x, this.y + offsetY);
+        this.setVisible(false);
+        this.setActive(false);
+      }       
+
+      this.scene.add.existing(this); 
+    }
+
     showOptionDialog(nextDialog){
       if (this.b){
         this.setVisible(false);
@@ -71,34 +94,34 @@ export default class DialogBox extends Phaser.GameObjects.Container{
       }       
       else{
         this.reset();
-        this.parrafo = nextDialog.dialogs;
+        this._parrafo = nextDialog.dialogs;
         this.setVisible(true);
         this.setActive(true);
       }       
     }
 
     reset(){
-      this.actParrafo = '';
-      this.cont = 0;
-      this.delay = 0;
-      this.textoEscrito = false;
-      this.parrafo = this.dm.getActualNode().dialogs;
+      this._actParrafo = '';
+      this._cont = 0;
+      this._delay = 0;
+      this._textoEscrito = false;
+      this._parrafo = this.dm.getActualNode().dialogs;
     }
 
     preUpdate(t, dt) {
-        if (!this.textoEscrito){
-            if (this.delay <= 0){
-              if (this.cont < this.parrafo.length){
-                this.actParrafo += this.parrafo[this.cont];
-                this.text.setText(this.actParrafo);             
-                this.cont++;
-                this.delay = this.textSpeed;
+        if (!this._textoEscrito){
+            if (this._delay <= 0){
+              if (this._cont < this._parrafo.length){
+                this._actParrafo += this._parrafo[this._cont];
+                this._text.setText(this._actParrafo);             
+                this._cont++;
+                this._delay = this._textSpeed;
               }
               else{
-                this.textoEscrito = true;
+                this._textoEscrito = true;
               }
             }    
-            else this.delay -= dt;
+            else this._delay -= dt;
           }
       }
  
