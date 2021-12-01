dcd2225ddb33c77b4b35426f7dd054be3fadff15
Sebana02 <sebana@ucm.es>
2021-12-01 23:22:14 +0100

    comentada match_scene.js

--- src/match_scene.js
+++ src/match_scene.js
@@ -1,13 +1,19 @@
 import Background from "./background.js";
 
-
+/**
+ * Escena que se muestra tras acabar la cita
+ */
 export default class MatchScene extends Phaser.Scene {
 
     constructor() {
       super({ key:'matchScene'});
     }
 
+    /**
+     * @param {object} data 
+     */
     init(data){
+      //match del nodo terminal
       this.match = data.match;
     }
 
@@ -15,11 +21,17 @@ export default class MatchScene extends Phaser.Scene {
       
       let canvasW = document.getElementById("juego").width;
       let canvasH = document.getElementById("juego").height;
-      this.background = new Background(this,canvasW/2, canvasH/2); 
+      this.background = new Background(this,canvasW/2, canvasH/2, 'fondoNegro'); 
       let textString = "La cita ha terminado. Match: " + this.match + "%";
       this.setText(canvasW/2, canvasH/2, textString);
     }
 
+    /**
+     * Dibuja texto en pantalla
+     * @param {number} x - posicion en el eje x
+     * @param {number} y - posicion en el eje y
+     * @param {string} string - texto a escribir
+     */
     setText(x, y, string){
       this.text = this.add.text(x, y, string);
       this.text.setOrigin(0.5,0.5);
