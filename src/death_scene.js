e041875e42a4610509fd020d6cf9ab79c1e9939e
jgomez18UCM <jgomez18@ucm.es>
2021-12-01 21:39:21 +0100

    cambio nombre death scene y comentado

--- src/death_scene.js
+++ src/death_scene.js
@@ -1,14 +1,19 @@
 import Background from "./background.js";
 
+/**
+ * Escena del final en el que matas a tu cita.
+ */
 export default class DeathScene extends Phaser.Scene {
 
     constructor() {
       super({ key:'deathScene'});
     }
+
     create(){
       
       let canvasW = document.getElementById("juego").width;
       let canvasH = document.getElementById("juego").height;
+      //Creamos el background y el texto de muerte.
       this.background = new Background(this,canvasW/2, canvasH/2);     
       this.sprite = this.add.sprite(canvasW/2, canvasH/2, 'muerta');
     }
