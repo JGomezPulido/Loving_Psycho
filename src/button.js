9b5dfb3c6bcaa7ec51bc3e798edbb59e9c4eb06f
jgomez18UCM <jgomez18@ucm.es>
2021-12-01 21:38:55 +0100

    comentado button

--- src/button.js
+++ src/button.js
@@ -1,13 +1,17 @@
+/**
+ * Representa un boton de un menú.
+ */
 export default class Button extends Phaser.GameObjects.Sprite {
 
   constructor(scene, x, y) {
+    
     super(scene, x, y, 'boton');
+
     this.scene.add.existing(this).setInteractive();
+
     this.on("pointerdown", () =>{
       this.scene.spawn()
       this.off("pointerdown");
     });
   }
-  
-  
 }
