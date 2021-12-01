b2873f62f000898258ec270b984abc95c90698a1
jgomez18UCM <jgomez18@ucm.es>
2021-12-01 21:37:58 +0100

    comentado background

--- src/background.js
+++ src/background.js
@@ -1,9 +1,16 @@
+/**
+ * Representa el fondo de la cita.
+ */
 export default class Background extends Phaser.GameObjects.Image{
     constructor(scene, x, y,){
         super(scene, x, y, 'fondoNegro');
         this.scene.add.existing(this);
     }
-        
+    
+    /**
+     * Este método sirve para cambiar la imagen del Background.
+     * @param {string} background - El nuevo fondo al que hay que cambiar. 
+     */
     changeBackground(background){
         this.setTexture(background);
     }
