cb1bdc52322a4fb3a08b5e047aed721e1666d979
Sebana02 <sebana@ucm.es>
2021-12-03 17:04:25 +0100

    Psychobar.js comentada

--- src/psycho_bar.js
+++ src/psycho_bar.js
@@ -1,22 +1,35 @@
+/**
+ * Representa la barra de instinto asesino
+ */
 export default class PsychoBar extends Phaser.GameObjects.Sprite{
+    /**
+     * Construye un nuevo objeto PsychoBar
+     * @param {Phaser.Scene} scene 
+     * @param {number} x Posición en el eje x
+     * @param {number} y Posición en el eje y
+     */
     constructor(scene, x, y){
         super(scene, x, y, 'barraPsycho');
         this.setOrigin(0.5, 1);
-        this.scaleY = 0;
+        this._scaleY = 0;
         this._score = 0;
         this.scene.add.existing(this);
-        this.rect = this.scene.add.rectangle(x,y, this.width, this.height, 0, 0);
-        this.rect.setOrigin(0.5,1);
-        this.rect.setStrokeStyle(2, 0xFF0000);
+        this._rect = this.scene.add.rectangle(x,y, this.width, this.height, 0, 0);
+        this._rect.setOrigin(0.5,1);
+        this._rect.setStrokeStyle(2, 0xFF0000);
         
 
         this.scene.events.on('changePsychoBar', this.changePsychoBar, this);
         this.scene.events.on('writtenText', this.prematureFillBar, this);
-        this.scene.events.on('dialogBoxClicked', this.barraTope, this);
-        this.tween = this.scene.tweens.add({
+        this.scene.events.on('dialogBoxClicked', this.fullBar, this);
+        this._tween = this.scene.tweens.add({
             targets: [] })
     }
 
+    /**
+     * Cambia el valor de la barra de instinto asesino
+     * @param {number} n cantidad a añadir a la barra
+     */
     changePsychoBar(n){
         this._score += n;
         
@@ -29,8 +42,8 @@ export default class PsychoBar extends Phaser.GameObjects.Sprite{
             this._score = 0;
         } 
         
-        this.tween.stop();
-        this.tween = this.scene.tweens.add({
+        this._tween.stop();
+        this._tween = this.scene.tweens.add({
             targets: [ this ],
             scaleY: this._score / 100,
             duration: 1000,
@@ -41,17 +54,23 @@ export default class PsychoBar extends Phaser.GameObjects.Sprite{
         this.scene.events.emit('changeBlood');
     }
 
-    barraTope(){
+    /**
+     * Método que comprueba si la barra ha llegado al tope y lanza el final malo
+     */
+    fullBar(){
         if (this._score === 100){
             this.scene.events.emit('badEnding');
         }
     }
 
+    /**
+     * Método que reduce el valor de la barra al usar una pastilla
+     */
     pillEffect(){
         this._score /= 2;
 
-        this.tween.stop();
-        this.tween = this.scene.tweens.add({
+        this._tween.stop();
+        this._tween = this.scene.tweens.add({
             targets: [ this ],
             scaleY: this._score / 100,
             duration: 3000,
@@ -62,11 +81,18 @@ export default class PsychoBar extends Phaser.GameObjects.Sprite{
         this.scene.events.emit('changeBlood');
     }
 
+    /**
+     * Método que cancela la animación de llenado de la barra y lo pone a la altura correspondiente
+     */
     prematureFillBar(){
-        this.tween.stop();
-        this.scaleY = this._score / 100;
+        this._tween.stop();
+        this._scaleY = this._score / 100;
     }
 
+    /**
+     * Devuelve el valor de la barra
+     * @returns {number} 
+     */
     getScore(){
         return this._score;
     }
