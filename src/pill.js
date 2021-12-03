c70c570a6123a79a1bfb6ee10388f78936e70e69
Sebana02 <sebana@ucm.es>
2021-12-03 10:54:45 +0100

    pill.js refactorizada y comentada

--- src/pill.js
+++ src/pill.js
@@ -1,34 +1,48 @@
+/**
+ * Respresenta las pastillas que bajan la barra de instinto asesino
+ */
 export default class Pill extends Phaser.GameObjects.Container{
 
+    /**
+     * Construye un nuevo objecto Pill
+     * @param {Phaser.scene} scene 
+     * @param {number} x - posición en el eje x
+     * @param {number} y - posición en el eje y
+     */
     constructor(scene, x, y){
         
-        
         super(scene,x,y);
-        this.sprite = scene.add.sprite(0, 10, 'pastilla')
-        this.sprite.setScale(0.12, 0.12);
-        this.add(this.sprite);
+        this._sprite = scene.add.sprite(0, 10, 'pastilla')
+        this._sprite.setScale(0.12, 0.12);
+        this.add(this._sprite);
         this.scene.add.existing(this);
-        this.sprite.setInteractive();
-        this.amount = 3;
-        this.text = scene.add.text(50,50,this.amount);
-        this.text.setFontSize(25);
-        this.text.setFontStyle('bold');
-        this.add(this.text);
-        this.bar = this.scene.psychoBar;
-        this.sprite.on('pointerdown', this.takePill, this);
-        this.space = this.scene.input.keyboard.addKey(' ');
-        this.space.on('down', this.takePill, this);
+        this._sprite.setInteractive();
+
+        this._amount = 3;
+
+        this._text = scene.add.text(50,50,this._amount);
+        this._text.setFontSize(25);
+        this._text.setFontStyle('bold');
+        this.add(this._text);
+
+        this._bar = this.scene.psychoBar;
+
+        this._sprite.on('pointerdown', this.takePill, this);
+        this._space = this.scene.input.keyboard.addKey('SPACE');
+        this._space.on('down', this.takePill, this);
     }
 
+    /**
+     * Método que se llama al tomarse una pastilla 
+     */
     takePill(){
-        if(this.amount>0){
-            console.log("A");
-            this.bar.pillEffect();
-            this.amount--;
-            this.text.text = this.amount;
+        if(this._amount>0){
+            this._bar.pillEffect();
+            this._amount--;
+            this._text.text = this._amount;
         }else{
-            this.sprite.off("pointerdown");
-            this.space.off('down');
+            this._sprite.off("pointerdown");
+            this._space.off('down');
         }
     }
     
