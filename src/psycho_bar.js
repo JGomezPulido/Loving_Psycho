4f8efb4d61fc3cfbf727d3ef7777a6fe1be0de66
Edu-git1 <eorellan@ucm.es>
2021-12-22 18:45:34 +0100

    Fixed detune

--- src/psycho_bar.js
+++ src/psycho_bar.js
@@ -63,13 +63,9 @@ export default class PsychoBar extends Phaser.GameObjects.Sprite {
         this._tween.once('complete', () => {
             this.fullBar();
         });
-        if (this._score >= 50){
-            this._detune = -10*this._score;
-        }
-        else{
-            this._detune = -5*this._score;
-        }
+        this._detune = -5*this._score;
         this.scene.events.emit('detuneMusic', this._detune);
+
         this.scene.events.emit('changeBlood');
        
     }
@@ -98,12 +94,7 @@ export default class PsychoBar extends Phaser.GameObjects.Sprite {
             yoyo: false,
             repeat: 0
         });
-        if (this._score >= 50){
-            this._detune = -10*this._score;
-        }
-        else{
-            this._detune = -5*this._score;
-        }
+        this._detune = -5*this._score;
         this.scene.events.emit('detuneMusic', this._detune);
         this.scene.events.emit('changeBlood');  
     }
