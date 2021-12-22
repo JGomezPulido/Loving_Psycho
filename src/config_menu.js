67ca1fcbc05da8ef369be5fe828d4728ac0a22f9
jgomez18UCM <jgomez18@ucm.es>
2021-12-22 17:54:07 +0100

    Botones del configMenu se pueden mantener pulsados

--- src/config_menu.js
+++ src/config_menu.js
@@ -14,10 +14,10 @@ export default class ConfigMenu extends Phaser.GameObjects.Container{
     constructor(scene, x, y, config){
         super(scene, x ,y);
 
-        this.sprite = this.scene.add.sprite(0,0,'pauseMenu');
+        this._sprite = this.scene.add.sprite(0,0,'pauseMenu');
         this._textVelocity = config.textVelocity;
 
-        this.add(this.sprite);
+        this.add(this._sprite);
         this.initVolumeConfig();
         this.initTextVelocity();
         this.initExit();
@@ -28,90 +28,107 @@ export default class ConfigMenu extends Phaser.GameObjects.Container{
     }
 
     initVolumeConfig(){
-        this.volumeContainer = this.scene.add.container(0, -190);
-        this.add(this.volumeContainer);
+        this._volumeContainer = this.scene.add.container(0, -190);
+        this.add(this._volumeContainer);
 
-        this.volumeContainer.sprite=this.scene.add.sprite(0,0,'boton').setScale(0.75,0.75);
-        this.volumeContainer.add(this.volumeContainer.sprite);
+        this._volumeContainer.sprite=this.scene.add.sprite(0,0,'boton').setScale(0.75,0.75);
+        this._volumeContainer.add(this._volumeContainer.sprite);
         
-        this.volumeContainer.upSelector = new Button(this.scene, 70,0,'heartPauseButton','+',0.6,0.6);
-        this.volumeContainer.add(this.volumeContainer.upSelector);
-        this.volumeContainer.upSelector.sprite.on('pointerdown', () => {
-            if(this.scene.game.sound.volume <= 0.99){
-                this.scene.game.sound.volume+=0.01;
-                this.volumeContainer.text.text = Phaser.Math.RoundTo(this.scene.game.sound.volume * 100,0);
-            }
+        this._volumeUp = false;
+        this._volumeContainer.upSelector = new Button(this.scene, 70,0,'heartPauseButton','+',0.6,0.6);
+        this._volumeContainer.add(this._volumeContainer.upSelector);
+        this._volumeContainer.upSelector.sprite.on('pointerdown', () => {
+            this._volumeUp = true;
+        });
+        this._volumeContainer.upSelector.sprite.on('pointerup', () => {
+            this._volumeUp = false;
+        });
+        this._volumeContainer.upSelector.sprite.on('pointerout', () => {
+            this._volumeUp = false;
         });
 
-        this.volumeContainer.downSelector = new Button(this.scene, -70,0,'heartPauseButton','-',0.6,0.6);
-        this.volumeContainer.add(this.volumeContainer.downSelector);
-        this.volumeContainer.downSelector.sprite.on('pointerdown', () => {
-            if(this.scene.game.sound.volume >= 0.01){
-                this.scene.game.sound.volume-=0.01; 
-                this.volumeContainer.text.text = Phaser.Math.RoundTo(this.scene.game.sound.volume * 100,0);
-            }
+        this._volumeDown = false;
+        this._volumeContainer.downSelector = new Button(this.scene, -70,0,'heartPauseButton','-',0.6,0.6);
+        this._volumeContainer.add(this._volumeContainer.downSelector);
+        this._volumeContainer.downSelector.sprite.on('pointerdown', () => {
+            this._volumeDown = true;
+        });
+        this._volumeContainer.downSelector.sprite.on('pointerup', () => {
+            this._volumeDown = false;
+        });
+        this._volumeContainer.downSelector.sprite.on('pointerout', () => {
+            this._volumeDown = false;
         });
 
-        this.volumeContainer.text = this.scene.add.text(5,0,(Phaser.Math.RoundTo(this.scene.game.sound.volume * 100,0)));
-        this.volumeContainer.add(this.volumeContainer.text);
-        this.volumeContainer.text.setOrigin(0.5,0.5);
+        this._volumeContainer.text = this.scene.add.text(5,0,(Phaser.Math.RoundTo(this.scene.game.sound.volume * 100,0)));
+        this._volumeContainer.add(this._volumeContainer.text);
+        this._volumeContainer.text.setOrigin(0.5,0.5);
 
-        this.volumeContainer.title = this.scene.add.text(0,50,'Volumen');
-        this.volumeContainer.add(this.volumeContainer.title);
-        this.volumeContainer.title.setColor('#000').setFontStyle('Bold').setOrigin(0.5,0.5).setFontSize(20);
+        this._volumeContainer.title = this.scene.add.text(0,50,'Volumen');
+        this._volumeContainer.add(this._volumeContainer.title);
+        this._volumeContainer.title.setColor('#000').setFontStyle('Bold').setOrigin(0.5,0.5).setFontSize(20);
 
     }
 
 
     initTextVelocity(){
-        this.textVelContainer = this.scene.add.container(0, -65);
-        this.add(this.textVelContainer);
+        this._textVelContainer = this.scene.add.container(0, -65);
+        this.add(this._textVelContainer);
 
-        this.textVelContainer.sprite=this.scene.add.sprite(0,0,'boton').setScale(0.7,0.7);
-        this.textVelContainer.add(this.textVelContainer.sprite);
+        this._textVelContainer.sprite=this.scene.add.sprite(0,0,'boton').setScale(0.7,0.7);
+        this._textVelContainer.add(this._textVelContainer.sprite);
         
-        this.textVelContainer.upSelector = new Button(this.scene, 70,0,'heartPauseButton','+',0.6,0.6);
-        this.textVelContainer.add(this.textVelContainer.upSelector);
-        this.textVelContainer.upSelector.sprite.on('pointerdown', () => {
-            if(this._textVelocity <= 99){
-                this._textVelocity+=1;
-                this.textVelContainer.text.text = this._textVelocity;
-            }
+        this._textVelUp = false;
+        this._textVelContainer.upSelector = new Button(this.scene, 70,0,'heartPauseButton','+',0.6,0.6);
+        this._textVelContainer.add(this._textVelContainer.upSelector);
+        this._textVelContainer.upSelector.sprite.on('pointerdown', () => {
+            this._textVelUp = true;
+        });
+        this._textVelContainer.upSelector.sprite.on('pointerup', () => {
+            this._textVelUp = false;
+        });
+        this._textVelContainer.upSelector.sprite.on('pointerout', () => {
+            this._textVelUp = false;
         });
 
-        this.textVelContainer.downSelector = new Button(this.scene, -70,0,'heartPauseButton','-',0.6,0.6);
-        this.textVelContainer.add(this.textVelContainer.downSelector);
-        this.textVelContainer.downSelector.sprite.on('pointerdown', () => {
-            if(this._textVelocity >= 1){
-                this._textVelocity-=1; 
-                this.textVelContainer.text.text = this._textVelocity;
-            }
+        this._textVelDown = false;
+        this._textVelContainer.downSelector = new Button(this.scene, -70,0,'heartPauseButton','-',0.6,0.6);
+        this._textVelContainer.add(this._textVelContainer.downSelector);
+        this._textVelContainer.downSelector.sprite.on('pointerdown', () => {
+            this._textVelDown = true;            
+        });
+        this._textVelContainer.downSelector.sprite.on('pointerup', () => {
+            this._textVelDown = false;
+        });
+        this._textVelContainer.downSelector.sprite.on('pointerout', () => {
+            this._textVelDown = false;
         });
 
-        this.textVelContainer.text = this.scene.add.text(5,0,this._textVelocity);
-        this.textVelContainer.add(this.textVelContainer.text);
-        this.textVelContainer.text.setOrigin(0.5,0.5);
 
-        this.textVelContainer.title = this.scene.add.text(0,50,'Velocidad Texto');
-        this.textVelContainer.add(this.textVelContainer.title);
-        this.textVelContainer.title.setColor('#000').setFontStyle('Bold').setOrigin(0.5,0.5).setFontSize(20);
+        this._textVelContainer.text = this.scene.add.text(5,0,this._textVelocity);
+        this._textVelContainer.add(this._textVelContainer.text);
+        this._textVelContainer.text.setOrigin(0.5,0.5);
+
+        this._textVelContainer.title = this.scene.add.text(0,50,'Velocidad Texto');
+        this._textVelContainer.add(this._textVelContainer.title);
+        this._textVelContainer.title.setColor('#000').setFontStyle('Bold').setOrigin(0.5,0.5).setFontSize(20);
 
 
     };
     initExit(){
 
-        this.esc = this.scene.input.keyboard.addKey("ESC");
-        this.esc.on('down', () => {
+        this._esc = this.scene.input.keyboard.addKey("ESC");
+        this._esc.on('down', () => {
             this.setActive(false);
             this.setVisible(false);
            
         })
         
-        this.exitButton = new Button(this.scene, 0, 150, 'boton', 'Exit', 0.95, 0.95);
-        this.add(this.exitButton);
+        this._exitButton = new Button(this.scene, 0, 150, 'boton', 'Exit', 0.95, 0.95);
+        this.add(this._exitButton);
 
-        this.exitButton.sprite.setInteractive()
-        this.exitButton.sprite.on('pointerdown', () => {
+        this._exitButton.sprite.setInteractive()
+        this._exitButton.sprite.on('pointerdown', () => {
             this.setActive(false);
             this.setVisible(false);
            
@@ -125,10 +142,30 @@ export default class ConfigMenu extends Phaser.GameObjects.Container{
     }
 
     getTextVelocity(){
-        return this._textVelocity;
+        return Phaser.Math.RoundTo(this._textVelocity);
     }
 
     setTextVelocity(vel){
         this._textVelocity = vel;
+    }
+
+    preUpdate(t, dt){
+        if(this._textVelDown && this._textVelocity >= 1){                    
+            this._textVelocity-= 8*(dt/1000); 
+            this._textVelContainer.text.text = Phaser.Math.RoundTo(this._textVelocity);  
+        }
+        if(this._textVelUp && this._textVelocity <= 99){
+            this._textVelocity+= 8*(dt/1000);
+            this._textVelContainer.text.text = Phaser.Math.RoundTo(this._textVelocity);
+        }      
+        if(this._volumeDown && this.scene.game.sound.volume >= 0.01){
+            this.scene.game.sound.volume-=0.015 * (dt/100); 
+            this._volumeContainer.text.text = Phaser.Math.RoundTo(this.scene.game.sound.volume * 100,0);
+        }
+        if(this._volumeUp && this.scene.game.sound.volume <= 0.99){
+            this.scene.game.sound.volume+=0.015 * (dt/100); 
+            this._volumeContainer.text.text = Phaser.Math.RoundTo(this.scene.game.sound.volume * 100,0);
+        }
+        
     }
 }
